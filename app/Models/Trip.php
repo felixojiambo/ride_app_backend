aa99d6f77d9cc7e84f1543e02be9881ac4b5dac9
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-11 06:46:25 -0700

    relationships in trip model"

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -9,4 +9,10 @@ class Trip extends Model
 {
     use HasFactory;
     protected $guarded=[];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function driver(){
+        return $this->belongsTo(Driver::class);
+    }
 }
