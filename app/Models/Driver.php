e9ab5aba9b4ac044a53bc4fb8b1882b80d1a388f
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-11 06:44:57 -0700

    relationships in driver  model

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -9,5 +9,11 @@ class Driver extends Model
 {
     use HasFactory;
     protected $guarded=[];
-   
+  public function user(){
+    return $this->belongsTo(User::class);
+  } 
+  public function trips()
+  {
+    return $this->hasMany(Trip::class);
+  }
 }
