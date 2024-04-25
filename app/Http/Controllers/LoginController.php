b3adabf8e1ed8e83c3fa45e6d7bca75cb8325e6c
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-25 10:54:04 -0700

    notification for login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use  App\Models\User;
 class LoginController extends Controller
 {
-    public function submit()
+    public function submit(Request  $request )
     {
         
-        
+        //validate phone number
+  $request->validate([
+ 'phone'=>'required|numeric|min:10'
+  ]);
+
+
+        //find or create a user model
+$user=User::firstOrCreate([
+    'phone'=> $request->phone
+]);
+if(!$user){
+    return response()->json(['message'=>'Could not process a user with that phone number.'],402);
+}
+
+        //send a user one time code for use
+
+$user->notify();
+        //reurn back a response
+
+
     }
 }
