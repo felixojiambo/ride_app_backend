088dec0f8b3b3b34a520dbe398a70a210e5c65de
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-05 22:19:57 -0700

    show driver user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,9 +8,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('/login', [LoginController::class,'submit']);
-Route::post('/login/verify', [LoginController::class,'verify']);
-Route::group(['middleware'=>'auth:sanctum'],function(){
-    Route::get('/user',function (Request $request) { return $request->user();
+Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::get('/driver', [DriverController::class,'show']);
+    Route::post('/driver', [DriverController::class,'update']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
     });
 });
