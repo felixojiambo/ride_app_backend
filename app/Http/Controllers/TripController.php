ec1099cbd9edc94eb377c037cc6659147996b8a6
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-05 22:39:00 -0700

    create trip and return to user

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -7,4 +7,20 @@ use Illuminate\Http\Request;
 class TripController extends Controller
 {
     //
+    public function store(Request $request){
+
+        $request->validate([
+            'origin'=> 'required',
+            'destination'=> 'required',
+
+            'destination_name'=> 'required',
+        ]
+        );
+      return   $request->user()->trips()->create($request->only([
+            'origin',
+            'destination',
+            'destination_name',
+
+        ]));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/driver', [DriverController::class,'show']);
     Route::post('/driver', [DriverController::class,'update']);
 
-    Route::post('/trip', [TripController::class,'']);
+    Route::post('/trip', [TripController::class,'store']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
